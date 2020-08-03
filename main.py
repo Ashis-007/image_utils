41fa5d7c23ba9840eed0f15d10d8802a50f65363
Ashis-007 <ashisjc@gmail.com>
2020-08-03 22:23:40 +0530

    Compression and changing ext done

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+from sys import exit
+from PIL import Image
 
 
 def show_menu():
@@ -11,15 +13,36 @@ def show_menu():
 
 
 def get_directory():
-    return repr(input("Enter the directory location(absolute path): "))
+    return input("Enter the directory location(absolute path): ")
 
 
 def change_extension(path, ext):
-    pass
-
+    os.chdir(path)
+    if not os.path.exists(os.path.join(path, "newImages")):
+        os.mkdir("newImages")    
+    for file in os.listdir():
+        try:
+            img = Image.open(file)
+            filename = os.path.splitext(file)[0]
+            img.save(f"newImages\\{filename}.{ext}")
+        except Exception as err:
+            print("Something went wrong!", err)
+            
 
 def compress_image(path):
-    pass
+    os.chdir(path)
+    if not os.path.exists(os.path.join(path, "compressed")):
+        os.mkdir("compressed")
+    factor = 0.5
+    for file in os.listdir():
+        try:
+            img = Image.open(file)
+            l, b = img.size
+            img = img.resize((int(l * factor), int(b * factor)), Image.LANCZOS)
+            img.save(f"{path}\\compressed\\{file}", optimize=True, quality=85)
+        except Exception as err:
+            print("Something went wrong!", err)
+
 
 
 def watermark_image(path):
@@ -27,9 +50,34 @@ def watermark_image(path):
 
 
 if __name__ == "__main__":
-    choice = "q"
+    choice = ""
     while choice != "q" or choice != "quit":
         show_menu()
         choice = input("    Enter command: ")
+        choice = choice.lower()
+
+        if choice == "q":
+            continue
+
+        elif choice == "c":
+            path = get_directory()
+            print(path)
+            compress_image(path)
+
+        elif choice == "e":
+            path = get_directory()
+            ext = input("Enter the new extension(e.g. png): ")
+            change_extension(path, ext)
+
+        elif choice == "t":
+            pass
+
+        else:
+            print("Invalid command. Please try again.")
+
+    else:
+        print("Closing program...")
+        print("Program closed")
+        exit()
 
 
